fda1d17be0f79068be8149f4f38dd4e43192a01e
kumar <godunity3d@gmail.com>
2025-10-02 10:14:09 +0530

    h

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -8,8 +8,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace <repo-name> with your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio-vite/',
+  base: "/devportfolio/my-portfolio/"
+  // or base: "/<repo-folder>/" according to your structure
 })
+
