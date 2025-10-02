d18969f6a306e65880204aa23aa4b7194f3659fc
kumar <godunity3d@gmail.com>
2025-10-02 12:25:08 +0530

    df

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/devportfolio/my-portfolio/'   // 👈 this is critical
+  base: '/devportfolio/'   // 👈 this is critical
 })
 
 
